a9ebe804378f6d848600186c46ffa68a2a82438e
William <william@blueskylabs.com.au>
2023-07-02 22:39:50 +0800

    Base tutorial completed
    completed to the point of functions complete,
    ui design and ux features next to complete

--- WeatherPage.xaml.cs
+++ WeatherPage.xaml.cs
@@ -6,7 +6,8 @@ namespace MauiPractice;
 public partial class WeatherPage : ContentPage
 {
     public List<Models.List> WeatherList;
-
+    private double latitude;
+    private double longtitude;
     public WeatherPage()
     {
         InitializeComponent();
@@ -18,9 +19,61 @@ public partial class WeatherPage : ContentPage
     protected async override void OnAppearing()
     {
         base.OnAppearing();
-        var result = await ApiService.GetWeather(47.6829, -122.1209);
+        await GetLocation();
+        await GetWeatherDataByLocation(latitude, longtitude);
+
+    }
 
 
+    public async Task GetLocation()
+    {
+        var location = await Geolocation.GetLocationAsync();
+        longtitude = location.Longitude;
+        latitude = location.Latitude;
+
+    }
+
+    private async void TapLocation_Tapped(object sender, TappedEventArgs e)
+    {
+        await GetLocation();
+        await GetWeatherDataByLocation(latitude, longtitude);
+    }
+
+    public async Task GetWeatherDataByLocation(double  latitude, double longitude)
+    {
+        var result = await ApiService.GetWeather(latitude, longtitude);
+        UpdateUI(result);
+
+
+    }
+    
+    public async Task GetWeatherDataByCity(string city)
+    {
+        var result = await ApiService.GetWeatherByCity(city);
+
+        UpdateUI(result);
+
+    }
+
+    private async void ImageButton_Clicked(object sender, EventArgs e)
+    {
+       var result = await DisplayPromptAsync(
+            title: "", 
+            message: "", 
+            placeholder: "Search weather by city",
+            accept: "Search",
+            cancel: "Cancel"
+            );
+        if( result != null)
+        {
+            await GetWeatherDataByCity( result );
+
+        }
+
+    }
+
+    public void UpdateUI(dynamic result)
+    {
         foreach (var List in result.list)
         {
             WeatherList.Add(List);
@@ -34,6 +87,6 @@ public partial class WeatherPage : ContentPage
         LblHumidity.Text = result.list[0].main.humidity + "%";
         LblWind.Text = result.list[0].wind.speed + " km/hr";
         ImgWeatherIcon.Source = result.list[0].weather[0].fullIconUrl;
-        
     }
+
 }
